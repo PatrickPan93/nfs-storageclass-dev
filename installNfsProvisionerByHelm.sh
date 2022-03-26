44d53770fe6a74ce48aab4fdb372ac3235fdeeb2
Administrator <admin@example.com>
2022-03-26 16:40:15 +0800

    fix

--- installNfsProvisionerByHelm.sh
+++ installNfsProvisionerByHelm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 helm repo add azure http://mirror.azure.cn/kubernetes/charts/
 helm install nfs-storage azure/nfs-client-provisioner \
---set nfs.server=172.16.130.50 \
---set nfs.path=/nfs-data \
+--set nfs.server=172.16.130.60 \
+--set nfs.path=/data \
 --set storageClass.name=nfs-storage \
 --set storageClass.defaultClass=true
